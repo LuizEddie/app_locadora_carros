fe04a77800e3507e8351a2093e404e67b1c44227
LuizEddie <contato.luiz1997@gmail.com>
2023-10-04 14:19:39 -0300

    Aula 298: Injeção de Model

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,14 +7,23 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+
+    private $marca;
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+    
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        return Marca::all();
+    {   
+        $marcas = $this->marca->all();
+        return $marcas;
+        // return Marca::all();
     }
 
     /**
@@ -35,19 +44,20 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        // $marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
-    {
-        return $marca;
+    public function show($id)
+    {   
+        return $this->marca->find($id);
     }
 
     /**
@@ -65,22 +75,26 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
+        $marca = $this->marca->find($id);
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
-    {
-        //
+    public function destroy($id)
+    {   
+        $marca = $this->marca->find($id);
+        $marca->delete($id);
+        return ['msg'=>'A marca foi removida com sucesso!'];
     }
 }
